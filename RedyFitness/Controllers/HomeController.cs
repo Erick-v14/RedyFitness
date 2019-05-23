97a95e4da6bc89a14e936d706a7b8b98e2c04220
Erick Valle <erick.t.valle@outlook.com>
2019-05-23 12:13:44 -0400

    created logo added contact info

--- RedyFitness/Controllers/HomeController.cs
+++ RedyFitness/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace RedyFitness.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Erick Valle";
 
             return View();
         }
