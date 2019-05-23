283cf41b9021845fbd8e63021f516ffdb2adfd94
Erick Valle <erick.t.valle@outlook.com>
2019-05-23 11:47:54 -0400

    Add project files.

---
+++ RedyFitness/Dto/Entry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedyFitness.Dto
+{
+    public class Entry
+    {
+        //add entry class
+        public int id { get; set; }
+      
+        public DateTime Date { get; set; }
+
+        public int Exercise { get; set; }
+
+
+        public int numOfReps { get; set; }
+        
+        public double numOfWeight { get; set; }
+
+        public string notes { get; set; }
+
+        public Entry ToModel()
+        {
+            return new Entry()
+            {
+                id = id,
+                Date = Date,
+                Exercise = Exercise,
+                numOfReps = numOfReps,
+                numOfWeight = numOfWeight,
+                notes = notes
+            };
+        }
+
+
+    }
+}
