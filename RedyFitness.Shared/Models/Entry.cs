d28f5966276e86e102f44b8339180146b424e658
Erick Valle <erick.t.valle@outlook.com>
2019-05-28 14:52:30 -0400

    fd

---
+++ RedyFitness.Shared/Models/Entry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace RedyFitness.Shared.Models
+{
+   public class Entry
+    {
+        public Entry()
+        {
+
+        }
+        public int ExerciseID { get; set; }
+
+        public DateTime Date { get; set; }
+        public int Repititions { get; set; }
+        public decimal Weight { get; set; }
+        public string Notes { get; set; }
+
+
+
+    }
+}
---
+++ RedyFitness.Shared/Models/Workouts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedyFitness.Shared.Models
+{
+    class Workouts
+    {
+        public Workouts()
+        {
+            Entries = new List<Entry>();
+        }
+        public int ExerciseID { get; set; }
+        public string Name { get; set; }
+        public IList<Entry> Entries { get; set; }
+
+
+    }
+}
