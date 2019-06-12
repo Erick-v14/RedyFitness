397c7329020cbaa643d17b1f90cec9bec8046cc6
Erick Valle <erick.t.valle@outlook.com>
2019-06-12 15:33:13 -0400

    .

--- RedyFitness/Models/Workouts.cs
+++ RedyFitness/Models/Workouts.cs
@@ -4,15 +4,29 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace RedyFitness.Models
 {
     public class Workouts
     {
         public int id { get; set; }
-        public string name { get; set; }
+        public string Name { get; set; }
 
-        
+        [Required]
+        [Display(Name = "Reps")]
+        public int MaxReps { get; set; }
+
+        [Required]
+        [Display(Name = "Weight")]
+        public double MaxWeight { get; set; }
+
+        [Required]
+        [DataType(DataType.Date, ErrorMessage ="Please enter a valid date")]
+        public DateTime EntryDate { get; set; }
+
+
     }
     public class LogWorkoutModel
     {
